1d9c0621ecf1fd19aea60686d7e42a9f8c849e7a
CarlosMendozaGarcia <mendozagarcia@uninorte.edu.co>
2026-08-18 23:39:44 -0500

    refactor (Footer): adding background for footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-
 export default function Home() {
   return (
-    <div className="flex-1 h-full w-full  content-center overflow-hidden p-4">
-      <h1 className="text-center text-primary text-display font-display font-bold">Bienvenidos a<br />Connect Latam VT</h1>
+    <div className="relative flex-1 h-full w-full  content-center overflow-hidden ">
+      <h1 className="text-center text-primary text-display font-display font-bold p-4">
+        Bienvenidos a<br />
+        Connect Latam VT
+      </h1>
+      
     </div>
   );
 }
--- shared/layout/Footer.tsx
+++ shared/layout/Footer.tsx
@@ -1,10 +1,23 @@
 import Foot from "@/public/Footer.svg";
 export default function Footer() {
   return (
-    <footer
-      className=" flex items-center justify-center shrink-0 h-[5dvh] w-full bg-primary  shadow-lg/50 shadow-secondary"
-    >
-      <p className=" text-headings-sm font-ui text-background">Developed by OrionByPixel</p>
+    <footer className="z-10 relative flex items-center justify-center shrink-0 h-[5dvh] w-full bg-primary  shadow-lg/50 shadow-secondary">
+      <div
+        className="absolute bottom-full shrink-0 h-[10dvh] w-full bg-primary"
+        style={{
+          maskImage: "url(/Footer.svg)",
+          WebkitMaskImage: "url(/Footer.svg)",
+          maskRepeat: "repeat-x",
+          WebkitMaskRepeat: "repeat-x",
+          maskSize: "auto 100%",
+          WebkitMaskSize: "auto 100%",
+          maskPosition: "center",
+          WebkitMaskPosition: "center",
+        }}
+      ></div>
+      <p className=" text-headings-sm font-ui text-background">
+        Developed by OrionByPixel
+      </p>
     </footer>
   );
 }
